550099e3efb1c730261a1837f3f36b41b852cdd4
ahamilto99 <ajhamilton15@hotmail.com>
2022-04-17 09:16:02 -0400

    Added SQL logging

---
+++ src/main/java/com/hamilton/j/alex/springgraphqlbatchloadingdemo/DataSourceQueryLoggingPostProcessor.java
@@ -0,0 +1,62 @@
+package com.hamilton.j.alex.springgraphqlbatchloadingdemo;
+
+import net.ttddyy.dsproxy.listener.logging.SLF4JLogLevel;
+import net.ttddyy.dsproxy.support.ProxyDataSource;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+import javax.sql.DataSource;
+
+@Component
+public class DataSourceQueryLoggingPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) {
+        if (bean instanceof DataSource && !(bean instanceof ProxyDataSource)) {
+            final var factory = new ProxyFactory(bean);
+
+            factory.setProxyTargetClass(true);
+            factory.addAdvice(new ProxyDataSourceInterceptor((DataSource) bean));
+
+            return factory.getProxy();
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(@NonNull Object bean, @NonNull String beanName) {
+        return bean;
+    }
+
+    private record ProxyDataSourceInterceptor(DataSource dataSource) implements MethodInterceptor {
+
+        private ProxyDataSourceInterceptor(final DataSource dataSource) {
+            this.dataSource = ProxyDataSourceBuilder
+                    .create(dataSource)
+                    .name("Kanban Database")
+                    .multiline()
+                    .logQueryBySlf4j(SLF4JLogLevel.INFO)
+                    .build();
+        }
+
+        @Override
+        public Object invoke(final MethodInvocation invocation) throws Throwable {
+            final var proxyMethod = ReflectionUtils.findMethod(this.dataSource.getClass(),
+                    invocation.getMethod().getName());
+
+            if (proxyMethod != null) {
+                return proxyMethod.invoke(this.dataSource, invocation.getArguments());
+            }
+
+            return invocation.proceed();
+        }
+
+    }
+
+}
