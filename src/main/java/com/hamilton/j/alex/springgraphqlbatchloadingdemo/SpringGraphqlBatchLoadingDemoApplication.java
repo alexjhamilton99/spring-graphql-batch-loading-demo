d4914a15213530fe8873a8a5d2c896b758e70c38
ahamilto99 <ajhamilton15@hotmail.com>
2022-04-17 07:50:15 -0400

    Initial commit

---
+++ src/main/java/com/hamilton/j/alex/springgraphqlbatchloadingdemo/SpringGraphqlBatchLoadingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hamilton.j.alex.springgraphqlbatchloadingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlBatchLoadingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlBatchLoadingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamilton/j/alex/springgraphqlbatchloadingdemo/SpringGraphqlBatchLoadingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamilton.j.alex.springgraphqlbatchloadingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlBatchLoadingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
