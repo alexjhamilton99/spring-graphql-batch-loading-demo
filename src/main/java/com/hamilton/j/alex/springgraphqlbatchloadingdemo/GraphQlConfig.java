4d400a25d6e4c8ec18ee05693a8fd5434182efd6
ahamilto99 <ajhamilton15@hotmail.com>
2022-04-17 09:06:31 -0400

    Configured date scalar

---
+++ src/main/java/com/hamilton/j/alex/springgraphqlbatchloadingdemo/GraphQlConfig.java
@@ -0,0 +1,16 @@
+package com.hamilton.j.alex.springgraphqlbatchloadingdemo;
+
+import graphql.scalars.ExtendedScalars;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Configuration
+public class GraphQlConfig {
+
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return runtimeWiringConfigurer -> runtimeWiringConfigurer.scalar(ExtendedScalars.Date);
+    }
+
+}
