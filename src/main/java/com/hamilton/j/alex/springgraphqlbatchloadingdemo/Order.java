5973987ea6fb77d79485044aa1783756a27b80c1
ahamilto99 <ajhamilton15@hotmail.com>
2022-04-17 08:30:07 -0400

    Added entities

---
+++ src/main/java/com/hamilton/j/alex/springgraphqlbatchloadingdemo/Order.java
@@ -0,0 +1,44 @@
+package com.hamilton.j.alex.springgraphqlbatchloadingdemo;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate orderDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+}
